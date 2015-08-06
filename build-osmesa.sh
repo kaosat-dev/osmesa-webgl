b3e57fe8c58f0622c2fe61dfcdf57932a209e559
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-08-06 17:45:24 +0200

    - updated all shell scripts so they work with more modern osmesa - added basic list of all needed system dependencies

--- build-osmesa.sh
+++ build-osmesa.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
 CORES=1
+MESA_VERSION="10.5.4"
+
+MESA_DIRNAME="mesa-$MESA_VERSION"
 
 if [[ -f /proc/cpuinfo ]]
 then
@@ -12,7 +15,7 @@ then
 	CORES=$(sysctl -n hw.ncpu 2>/dev/null || echo $CORES)
 fi
 
-pushd Mesa-10.1.0
+pushd  "$MESA_DIRNAME"
 ./configure --enable-gallium-osmesa --disable-egl --disable-driglx-direct --disable-dri --with-gallium-drivers=swrast --enable-gallium-llvm
 make -j $CORES
 popd
--- get-and-configure-osmesa.sh
+++ get-and-configure-osmesa.sh
@@ -1,22 +1,21 @@
 #!/bin/bash -e
 
-MESA_VERSION="10.1.0"
+MESA_VERSION="10.5.4"
 
-MESA_DIRNAME="Mesa-$MESA_VERSION"
-MESA_FILENAME="MesaLib-$MESA_VERSION.tar.bz2"
-MESA_URL="ftp://ftp.freedesktop.org/pub/mesa/current/MesaLib-$MESA_VERSION.tar.bz2"
+MESA_DIRNAME="mesa-$MESA_VERSION"
+MESA_FILENAME="mesa-$MESA_VERSION.tar.gz"
+MESA_URL="ftp://ftp.freedesktop.org/pub/mesa/current/mesa-$MESA_VERSION.tar.gz"
 
-if [[ ! -d "$MESA_DIRNAME" ]]
-then
-
-	if [[ ! -f "$MESA_FILENAME" ]]
-	then
-		curl -o "$MESA_FILENAME" "$MESA_URL"
-	fi
-
-	tar jxf "$MESA_FILENAME"
-fi
+#if [ ! -d "$MESA_DIRNAME" ] ; then
+#  echo "Not installed, fetching ..."
+#	if [ ! -f "$MESA_FILENAME" ]; then
+#    echo "no file "
+#	  curl -o "$MESA_FILENAME" "$MESA_URL"
+# fi
+#	tar zxvf "$MESA_FILENAME"
+#fi
 
 pushd "$MESA_DIRNAME"
-autoreconf -v --install
+  echo "installin'"
+  autoreconf -v --install
 popd
